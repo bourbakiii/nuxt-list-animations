14d93ab7a681e7ce6c29fc6584e10a36910019dd
bourbakii <gabisov03912@gmail.com>
2022-01-19 16:47:19 +0300

    Control point

--- components/Alerts/Index.vue
+++ components/Alerts/Index.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="wrapper alerts">
-
+      <AlertsItem v-for='(item, index) in items' :key='index' :item='item'/>
   </div>
 </template>
 
 <script>
 export default {
-
+    computed:{
+        items(){
+            return this.$store.state.alerts.items;
+        }
+    }
 }
 </script>
 
@@ -14,6 +18,9 @@ export default {
     .alerts{
         position: fixed; bottom:0px; right:0px;
         height:400px; width: 600px;
-        background-color: rgba($white,0.4);
-    }
+        border-radius: 15px 0px 0px 0px;
+        background-color: rgba(100,100,100,0.2);
+        backdrop-filter: blur(3px);
+        z-index: $modalsZIndex;
+        display: flex;align-items: center;justify-content: flex-end;flex-direction: column;    }
 </style>
--- components/Alerts/Item.vue
+++ components/Alerts/Item.vue
@@ -1,13 +1,21 @@
 <template>
-  
+  <div class="wrapper item alerts-item">
+      {{item}}
+  </div>
 </template>
 
 <script>
 export default {
-
+    props:{
+        item:{
+            required: true
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-
+    .item{
+        color:$red;
+    }
 </style>
