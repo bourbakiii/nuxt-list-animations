63dd41a74e7d1628f8be501885793e9343ff1bf2
bourbakii <gabisov03912@gmail.com>
2022-01-26 18:10:25 +0300

    Control point

--- pages/button.vue
+++ pages/button.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="page button-page">
-    <button @mouseleave="unhover" @mouseenter="hover" class="hover-button">
+    <button @mouseleave="unhover"  @mouseenter="hover" class="hover-button">
       HOVER ME
+      <span class="hover-button__bubble"></span>
     </button>
   </div>
 </template>
@@ -9,21 +10,27 @@
 <script>
 export default {
   name: "ButtonPage",
-  mounted() {
-    const elements = document.querySelectorAll(".hover-button");
-    elements.forEach((element) => {
-      const circle = document.createElement("div.hover-growing-circle");
-      element.appendChild(circle);
-    });
-  },
   methods: {
     hover(event) {
-      const element = event.target;
-      let child = element.childNodes[0]
-      console.log("hover");
+      let bubble = event.target.querySelector(".hover-button__bubble");
+      bubble.classList.add("action");
+      let [width, height] = [
+        event.target.getBoundingClientRect().width * 3,
+        event.target.getBoundingClientRect().height * 3,
+      ];
+      bubble.style.width = Math.max(width, height) + "px";
+      bubble.style.height = Math.max(width, height) + "px";
+      bubble.style.left = event.offsetX + "px";
+      bubble.style.top = event.offsetY + "px";
     },
-    unhover() {
-      console.log("unhover");
+
+    unhover(event) {
+      let bubble = event.target.querySelector(".hover-button__bubble");
+      bubble.classList.remove("action");
+      bubble.style.width = "0px";
+      bubble.style.height = "0px";
+      bubble.style.left = event.offsetX + "px";
+      bubble.style.top = event.offsetY + "px";
     },
   },
 };
@@ -33,6 +40,23 @@ export default {
   .hover-button {
     width: 90%;
     height: 83vh;
+    overflow: hidden;
+    background-color: transparent;
+    &__bubble {
+      position: absolute;
+      top: 0px;
+      left: 0px;
+      width: 0px;
+      height: 0px;
+      transform: translate(-50%, -50%);
+      background-color: red;
+      opacity: 0.5;
+        clip-path: circle();
+        transition: all 1s, top 0s, left 0s;
+      &.action {
+        opacity: 1;
+      }
+    }
   }
 }
 </style>
